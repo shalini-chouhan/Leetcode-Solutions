9b69e6369ee48837068e5a9b5237cd5ec6e86bc2
SHALINI CHOUHAN <72332766+shalini-chouhan@users.noreply.github.com>
2022-05-11 23:34:53 +0530

    Time: 7 ms (51.64%), Space: 12 MB (75.38%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if(!next_permutation(nums.begin(), nums.end()))
+            sort(nums.begin(), nums.end());
+    }
+};
